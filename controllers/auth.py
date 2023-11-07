62685051c14e4245f83b8a605f35e766524016f1
bipblop555 <tom.bonnet@hotmail.com>
2023-11-07 21:32:20 +0100

    user controller

---
+++ controllers/auth.py
@@ -0,0 +1,21 @@
+from flask_login import login_user, login_manager, logout_user, LoginManager
+
+from models.user import User
+
+
+def login(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
+    return None
+
+
+def setup_flask_login(app):
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
+    return login_manager
